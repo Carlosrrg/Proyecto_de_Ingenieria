a9be047c62a4a1ae26dcc02d4f1ab5730c51e94f
FaridLesther <FaridLesther@yahoo.es>
2020-04-20 22:44:42 -0600

    cambio en el header

--- HTML_de_website/good_shopping/index.php
+++ HTML_de_website/good_shopping/index.php
@@ -103,26 +103,20 @@
 			}
 			else{
 				$usuario = $_SESSION['codigo_usuario_sesion'];
-			    //echo "seccion iniciada por: " . $usuario;
+				//echo "seccion iniciada por: " . $usuario;
+				$conexion->establecerConexion();
+				$resultado_usuario = $conexion->ejecutarInstruccion("	SELECT NOMBRE
+																		FROM TBL_USUARIOS
+																		WHERE CODIGO_USUARIO = '$usuario'");
+				oci_execute($resultado_usuario);
+				while ($fila = $conexion->obtenerFila($resultado_usuario)) {
+					echo '<h6 style="padding-top:4px; margin-right:-10px;">'.$fila["NOMBRE"].'</h6>';
+				}
 			    echo'<div class="nav-item dropdown">';
 					echo'<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">';
-							$conexion->establecerConexion();
-							$resultado_usuario = $conexion->ejecutarInstruccion("	SELECT NOMBRE,CODIGO_TIPO_USUARIO
-																					FROM TBL_USUARIOS
-																					WHERE CODIGO_USUARIO = '$usuario'");
-							oci_execute($resultado_usuario);
-							while ($fila = $conexion->obtenerFila($resultado_usuario)) {
-							 	echo $fila["NOMBRE"];
-							 	$tipo_usuario = $fila["CODIGO_TIPO_USUARIO"];
-							}
 					echo'</a>';
-					echo'<div class="dropdown-menu" style="margin: 9px 0 0 -40px;">';
-						if ($tipo_usuario==1) {
-							echo'<a class="dropdown-item" href="Adm_gestion_publicaciones.php">Administrar</a>';
-						}
-						else {
-							echo'<a class="dropdown-item" href="Perfil_usuario_empresarial.php">Ver Perfil</a>';
-						}
+					echo'<div class="dropdown-menu" style="margin: 9px 0 0 -110px;">';
+						echo'<a class="dropdown-item" href="Perfil_usuario_empresarial.php">Ver Perfil</a>';
 						echo'<a class="dropdown-item" href="php/session_cerrar.php">Cerrar Sesión</a>';
 					echo'</div>';
 				echo'</div>';
