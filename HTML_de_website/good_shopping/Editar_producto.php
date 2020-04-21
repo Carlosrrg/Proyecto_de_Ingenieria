51faa3d8f7ecb8136c65041acb3d6b0d8dd01ac4
Carlosrrg <carlosrrg06@yahoo.com>
2020-04-21 12:59:11 -0600

    hipervinculo de notificaciones

--- HTML_de_website/good_shopping/Editar_producto.php
+++ HTML_de_website/good_shopping/Editar_producto.php
@@ -185,7 +185,7 @@
 
 			  			echo '<div class="list-group list-group-flush">';
 							echo '<a href="Productos_y_servicios.php" class="list-group-item list-group-item-action bg-light"><span><h6><i class="fas fa-shopping-bag"></i> Mis Productos</h6></span></a>'; 
-							echo '<a href="#" class="list-group-item list-group-item-action bg-light"><span>
+							echo '<a href="Notificaciones.php" class="list-group-item list-group-item-action bg-light"><span>
 								<h6><i class="fas fa-money-bill-alt"></i> Notificaciones</h6></span></a>';
 					  	echo '</div>';
 					  	
